7f2b96e0867947c5630e60b32e405e17ed0ff7af
ani0989 <ani.0989@gmail.com>
2020-08-18 10:47:50 -0700

    Updates

--- provider/provider.go
+++ provider/provider.go
@@ -1,1 +1,32 @@
 package provider
+
+import (
+	"github.com/ani0989/terraform-provider-example/api/client"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[String]*schema.Schema{
+			"username": {
+				Type:     schema.TypString,
+				Required: false,
+			},
+			"password": {
+				Type:     schema.TypString,
+				Required: false,
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{
+			"example_item": resourceItem(),
+		},
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	username := d.Get("username").(string)
+	password := d.Get("password").(string)
+	return client.NewBasicAuthClient(username, password), nil
+}
